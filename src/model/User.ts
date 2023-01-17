c76389d34d25aea868c6870688ba00964f48316b
Artur Vidor <vidorartur@gmail.com>
2023-01-17 11:55:37 -0300

    Complementando o Test

---
+++ src/model/User.ts
@@ -0,0 +1,9 @@
+export class User {
+  name: string
+  email: string
+
+  constructor () {
+    this.name = ''
+    this.email = ''
+  }
+}
--- tests/first.test.ts
+++ tests/first.test.ts
@@ -1,5 +1,10 @@
-describe('Fazendo um pequeno teste', function () {
-  test('Pequeno', function () {
+import { User } from '../src/model/User'
 
+describe('Apenas um teste para validar o funcionameto do Jest', function () {
+  test('Se a propriedade e a importação está funcionando', function () {
+    const user = new User()
+    user.name = 'Artur'
+
+    expect(user.name).toBe('Artur')
   })
 })
